3d1d4659d2a003b973b6c9401607abc7f058c362
marcelrenan-web <marcel.219184@fmm.org.br>
2025-11-15 17:25:11 -0300

    Delete create_table_denuncias.sql

--- create_table_denuncias.sql
+++
@@ -1,8 +0,0 @@
--- SQL para criar a tabela 'denuncias' (Postgres/Supabase)
-CREATE TABLE IF NOT EXISTS denuncias (
-    id TEXT PRIMARY KEY,
-    setor TEXT,
-    tipo_ocorrencia TEXT,
-    descricao TEXT,
-    data_envio TIMESTAMP WITH TIME ZONE DEFAULT NOW()
-);
