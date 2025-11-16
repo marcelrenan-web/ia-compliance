8f8624e6810291bec58d75c0829ee24d44e4507e
marcelrenan-web <marcel.219184@fmm.org.br>
2025-11-15 21:24:42 -0300

    Update sessao.py

--- utils/sessao.py
+++ utils/sessao.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
-def verificar_login():
-    return st.session_state.get("logado", False)
+def is_logged_in():
+    return st.session_state.get("auth_user") is not None
 
-def logout():
-    st.session_state.logado = False
-    st.switch_page("auth.py")
+def set_user(user_info):
+    st.session_state["auth_user"] = user_info
+
+def logout_user():
+    if "auth_user" in st.session_state:
+        del st.session_state["auth_user"]
